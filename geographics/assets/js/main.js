23c99b39f95cbfec0d4bea86c37dcea9ba83643b
StephanieHrvatin <shrv0001@student.monash.edu>
2021-06-15 19:50:26 +1000

    update

--- geographics/assets/js/main.js
+++ geographics/assets/js/main.js
@@ -9,12 +9,52 @@ $(document).ready(function () {
         $(this).closest(".panel").removeClass("active");
     });  
 
-    $(".earthbutton").click(function() {
+    $(".earth").click(function() {
       $(".earthpanel").addClass("active");
   });
 
   $(".close").click(function() {
     $(this).closest(".panel2").removeClass("active");
+}); 
+
+$(".cloud").click(function() {
+  $(".cloudpanel").addClass("active");
+});
+
+$(".close").click(function() {
+$(this).closest(".panel3").removeClass("active");
+}); 
+
+$(".city").click(function() {
+  $(".citypanel").addClass("active");
+});
+
+$(".close").click(function() {
+$(this).closest(".panel4").removeClass("active");
+}); 
+
+$(".address").click(function() {
+  $(".addresspanel").addClass("active");
+});
+
+$(".close").click(function() {
+$(this).closest(".panel5").removeClass("active");
+}); 
+
+$(".interface").click(function() {
+  $(".interfacepanel").addClass("active");
+});
+
+$(".close").click(function() {
+$(this).closest(".panel6").removeClass("active");
+}); 
+
+$(".user").click(function() {
+  $(".userpanel").addClass("active");
+});
+
+$(".close").click(function() {
+$(this).closest(".panel7").removeClass("active");
 }); 
     
   });
